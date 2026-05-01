37090cffe2b0401192a046efdb7ebfa18ef31c8d
Kealiiahonui Ohumukini <kealii.ohumukini@gmail.com>
2026-04-30 20:00:25 -0700

    Implemented: HTML Changes; CSS Styling Changes, JavaScript Features, Favicon.

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ window.addEventListener('load', () => {
     map.invalidateSize();
 });
 
+/*
+Optional for dark mode users
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; OpenStreetMap &copy; CARTO'
+}).addTo(map);
+
+*/
+
 // Legend Initialization
 const legend = L.control({ position: "bottomright" }); 
 
@@ -42,3 +51,42 @@ const filterBarToggle = document.getElementById("filter-toggle");
 filterBarToggle.addEventListener('click', () => {
     filterBar.classList.toggle('open');
 });
+
+// Section1 Dropdown
+const data = ["Washington", "Arizona", "Utah", "Wyoming", "Montana", "California", "Florida", "Maine", "New York"]; 
+const limit = 8; 
+
+const input = document.getElementById("station-search");
+const list = document.getElementById("station-list"); 
+
+function displayList(filterText = "") {
+    const filteredData = data.filter(item => item.toLowerCase().includes(filterText.toLowerCase())); 
+
+    let topEightResults = filteredData.slice(0, limit);
+    
+    list.innerHTML = ""; 
+
+    if (topEightResults.length > 0) {
+        // Reveals the list
+        list.classList.remove("hidden"); 
+        topEightResults.forEach(listItem => {
+            let li = document.createElement('li'); 
+            li.textContent = listItem; 
+            li.onclick = () => {
+                input.value = listItem; 
+                list.classList.add('hidden'); 
+            };
+            list.appendChild(li); 
+        });
+    } else {
+        list.classList.add('hidden'); 
+    }
+}
+
+input.addEventListener('input', (event) => displayList(event.target.value)); 
+
+document.addEventListener('click', (target) => {
+    if (target.target !== input) {
+        list.classList.add('hidden'); 
+    }
+})
